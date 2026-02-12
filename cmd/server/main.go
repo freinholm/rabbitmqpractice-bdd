6d17880c2804904dd0d6367f4812650611c814b7
Freinholm <aaron.frein@presidio.edu>
2026-02-12 11:31:11 -0800

    Wire fork up to be internally consistent, enable basic messaging via RabbitMQ

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/freinholm/rabbitmqpractice-bdd/internal/pubsub"
+	"github.com/freinholm/rabbitmqpractice-bdd/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+
+	rabbitMQ, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("Error connecting to RabbitMQ: %v", err)
+	}
+	defer rabbitMQ.Close()
+
+	fmt.Println("Connection to RabbitMQ is successful.")
+
+	publishChan, err := rabbitMQ.Channel()
+	if err != nil {
+		log.Fatalf("Could not create channel in RabbitMQ: %v", err)
+	}
+
+	err = pubsub.PublishJSON(
+		publishChan,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Printf("could not publish time: %v", err)
+	}
+	fmt.Println("Pause message sent!")
 }
